12dd90caf3eccdac46ca16491c85991b3b2c8752
Mako <DAGINATSUKO@users.noreply.github.com>
2017-02-22 22:50:33 -0500

    Delete ui-notify.php

--- module/ui-notify.php
+++
@@ -1,5 +0,0 @@
-<div id='alert-tx1-notify' class='div-notify'>
-	<p>
-		RPCS3.net is currently undergoing scheduled maintenance at this time. Page content may be temporarily broken or distorted.
-	</p>
-</div>
