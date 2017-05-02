769d7ab635c01e6a169b99987cedd958c24d43d7
DAGINATSUKO <DAGINATSUKO@users.noreply.github.com>
2017-05-01 21:36:57 -0400

    Add files via upload

--- lib/module/ui-menu.php
+++ lib/module/ui-menu.php
@@ -1,159 +1,70 @@
 <div id='menu-con-navbar'>
-	<a href='/'>
-	<div id='menu-ico-logo'>
-	</div>
-	</a>
-	<div id='menu-tx1-open'>
-		 MENU
-	</div>
-	<div id='menu-btn-open' title="Menu">
-	</div>
-	<div id='menu-con-donate'>
-		<div id='menu-tx1-button' title="Support us with Patreon or Alipay">
-			 Donate
-		</div>
-		<div id='menu-ico-donate'>
-		</div>
-	</div>
-	<a href="https://github.com/RPCS3/rpcs3" target="_blank">
-	<div id='menu-con-git'>
-		<div id='menu-tx1-button' title="Contribute with GitHub">
-			 GitHub
-		</div>
-		<div id='menu-ico-git'>
-		</div>
-	</div>
-	</a>
-</div>
-<!-- End -->
-<div id='menu-con-dim' class='toggle-menu'>
-	<div id='menu-con-anim' class='dropin-load'>
-		<div id="menu-tx1-title" style='-webkit-animation-duration: .4s;'>
-			 MAIN MENU
-		</div>
-			<div id="menu-con-menu">
-				<a href='/'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Home
-				</div>
-				<div id='menu-ico-button' class="ico-home" style="background:url('/img/icons/menu/home.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/blog'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Blog
-				</div>
-				<div id='menu-ico-button' class="ico-blog" style="background:url('/img/icons/menu/blog.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/team'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Team
-				</div>
-				<div id='menu-ico-button' class="ico-team" style="background:url('/img/icons/menu/team.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/roadmap'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Roadmap
-				</div>
-				<div id='menu-ico-button' class="ico-roadmap" style="background:url('/img/icons/menu/roadmap.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/compatibility'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Compatibility
-				</div>
-				<div id='menu-ico-button' class="ico-compatibility" style="background:url('/img/icons/menu/compatibility.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/quickstart'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Quickstart
-				</div>
-				<div id='menu-ico-button' class="ico-quickstart" style="background:url('/img/icons/menu/quickstart.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/download'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Download
-				</div>
-				<div id='menu-ico-button' class="ico-download" style="background:url('/img/icons/menu/download.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='/faq'>
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s; border-bottom: 1px solid rgba(0,0,0,.1);'>
-					 FAQ
-				</div>
-				<div id='menu-ico-button' class="ico-faqs" style="background:url('/img/icons/menu/faq.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='http://www.emunewz.net/forum/forumdisplay.php?fid=172' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Discussion
-				</div>
-				<div id='menu-ico-button' class="ico-discussion" style="background:url('/img/icons/menu/discussion.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='https://www.patreon.com/Nekotekina' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Patreon
-				</div>
-				<div id='menu-ico-button' class="ico-patreon" style="background:url('/img/icons/menu/patreon.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='https://discord.me/RPCS3' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Discord
-				</div>
-				<div id='menu-ico-button' class="ico-discord" style="background:url('/img/icons/menu/discord.png') no-repeat center; background-size: 20px;">
-				</div>
-				</a>
-				<a href='https://www.youtube.com/channel/UCz3-0QxNr4S4gK0xaWy7exQ' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 YouTube
-				</div>
-				<div id='menu-ico-button' class="ico-youtube" style="background:url('/img/icons/menu/youtube.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='https://twitter.com/rpcs3_emu' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Twitter
-				</div>
-				<div id='menu-ico-button' class="ico-twitter" style="background:url('/img/icons/menu/twitter.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='https://www.reddit.com/r/rpcs3/' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 Reddit
-				</div>
-				<div id='menu-ico-button' class="ico-reddit" style="background:url('/img/icons/menu/reddit.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<a href='https://github.com/RPCS3/rpcs3' target="_blank">
-				<div id="menu-btn-button" style='-webkit-animation-duration: .4s;'>
-					 GitHub
-				</div>
-				<div id='menu-ico-button' class="ico-github" style="background:url('/img/icons/menu/github.png') no-repeat center; background-size: 15px;">
-				</div>
-				</a>
-				<div id="menu-fil-footer">
-				</div>
-				<div id="menu-fil-footer">
-				</div>
-			</div>
-		<div id='menu-con-footer'>
-			<div id='menu-ovr-footer'>
-			</div>
-			<div id='menu-ico-footer'>
-			</div>
-			<a href="https://github.com/DAGINATSUKO/RPCS3-Website" target="_blank">
-			<div id='menu-tx1-footer'>
-				<div style="color: #002556 !important;" title="Website designed by DAGINATSUKO">
-					 Website source code on GitHub
-				</div>
+	<div id="menu-con-container">
+		<div id="menu-in-container">
+			<a href='/'>
+			<div id="menu-ico-logo" title="Home">
 			</div>
 			</a>
+			<a href='/blog'>
+			<div id="menu-btn-select" title="Blog">
+				Blog
+			</div>
+			</a>
+			<a href='/about'>
+			<div id="menu-btn-select" title="About">
+				About
+			</div>
+			</a>
+			<a href='/compatibility'>
+			<div id="menu-btn-select" title="Compatibility">
+				Compatibility
+			</div>
+			</a>
+			<a href='/download'>
+			<div id="menu-btn-select" title="Download">
+				Download
+			</div>
+			</a>
+			<a href='/quickstart'>
+			<div id="menu-btn-select" title="Quickstart">
+				Quickstart
+			</div>
+			</a>
+			<a href='/roadmap'>
+			<div id="menu-btn-select" title="Roadmap">
+				Roadmap
+			</div>
+			</a>
+			<a href='/faq'>
+			<div id="menu-btn-select" title="FAQ">
+				FAQ
+			</div>
+			</a>
+			<div id="menu-btn-select">
+				|
+			</div>
+			<a href='http://www.emunewz.net/forum/forumdisplay.php?fid=172' target="_blank">
+			<div id="menu-btn-select" title="Forum">
+				Forum
+			</div>
+			</a>
+			<a href='https://discord.me/RPCS3' target="_blank">
+			<div id="menu-btn-select" title="Discord">
+				Discord
+			</div>
+			</a>
+			<a href='https://github.com/RPCS3/rpcs3' target="_blank">
+			<div id="menu-btn-select" title="GitHub">
+				GitHub
+			</div>
+			</a>
+			<div id="menu-con-donate">
+				<div id="menu-tx1-button" title="Support us with Patreon or Alipay">
+					Support Us
+				</div>
+				<div id="menu-ico-donate">
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
