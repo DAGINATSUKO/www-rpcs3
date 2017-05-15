0999f334c8c4d5e24b129ec45e5d45595bdb64f5
DAGINATSUKO <daginatsuko@gmail.com>
2017-05-15 18:53:05 -0400

    Added files via upload

---
+++ public_html/lib/module/block-featurette.php
@@ -0,0 +1,19 @@
+<div id='featurette-con-container'>
+	<div id='featurette-tx1-body'>
+		 FEATURED PROGRESS
+	</div>
+	<div id='featurette-div-body'>
+	</div>
+	<div id='featurette-ico-body'>
+	</div>
+	<div id='featurette-ovr-body'>
+	</div>
+	<a href='https://discord.me/RPCS3' target="_blank">
+	<div id='featurette-btn-body' title="Talk about it on Discord!">
+	</div>
+	</a>
+	<div class="visual-wavebar-1">
+	</div>
+	<div class="visual-wavebar-1 delayed-fade" style="display:none;">
+	</div>
+</div>
---
+++ public_html/roadmap.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>RPCS3 - Roadmap</title>
+<meta charset="UTF-8">
+<meta name="description" content="RPCS3 is an open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows and Linux.">
+<meta name="keywords" content="rpcs3, ps3, PlayStation 3, emulator, nekotekina, roadmap">
+<meta name="author" content="RPCS3">
+<meta name="copyright" content="RPCS3">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/call-meta.php';?>
+<?php include 'lib/module/call-sys.php';?>
+</head>
+<body>
+<?php include 'lib/module/call-php.php';?>
+<div id="page-con-content">
+	<div id="header-con-head">
+		<div id='header-img-head' class="dynamic-banner">
+		</div>
+		<div id='header-con-overlay'>
+		</div>
+		<div id='header-con-body'>
+			<div id='header-tx1-body'>
+				<h1>ROADMAP</h1>
+			</div>
+			<div id='header-tx2-body'>
+				<p>
+					Structured roadmap for RPCS3 development
+				</p>
+			</div>
+		</div>
+	</div>
+	<div id="page-con-container">
+		<div id="page-in-container" class="div-roadmap-git">
+			<?php
+			$content = file_get_contents("https://github.com/RPCS3/rpcs3/wiki/Roadmap");
+			// Check for error fetching the page: if (!$content) { echo "something"; }
+			$first_step = explode("<div id=\"wiki-body\" class=\"wiki-body gollum-markdown-content instapaper_body\">", $content); 
+			$second_step = explode("</div>" , $first_step[1]); 
+			echo "<div id=\"wiki-body\" class=\"wiki-body gollum-markdown-content instapaper_body\">"; 
+			echo $second_step[0]; echo "</div></div>"; 
+			?>
+	</div>
+</div>
+<!-- End -->
+<?php include 'lib/module/ui-footer.php';?>
+<!-- End -->
+</div>
+</body>
+</html>
