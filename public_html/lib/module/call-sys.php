3f51abdace6bf0daffd06852ca7fb53b006494d3
Mako <DAGINATSUKO@users.noreply.github.com>
2017-08-27 09:49:01 -0400

    Removed compat.css from global call

--- public_html/lib/module/call-sys.php
+++ public_html/lib/module/call-sys.php
@@ -1,9 +1,8 @@
 <script src='/lib/js/api.js'></script>
 <script src='/lib/js/main.js'></script>
-<link rel="stylesheet" href="/lib/css/html.css?v=version2"/>
-<link rel="stylesheet" href="/lib/css/anim.css?v=version2"/>
-<link rel="stylesheet" href="/lib/css/main.css?v=version2"/>
-<link rel="stylesheet" href="/lib/css/menu.css?v=version2"/>
-<link rel="stylesheet" href="/lib/compat/compat.css?v=version2"/>
-<link rel="stylesheet" href="/lib/css/scale.css?v=version2"/>
-<link rel="stylesheet" href="/lib/css/debug.css?v=version2"/>
+<link rel="stylesheet" href="/lib/css/html.css"/>
+<link rel="stylesheet" href="/lib/css/anim.css"/>
+<link rel="stylesheet" href="/lib/css/main.css"/>
+<link rel="stylesheet" href="/lib/css/menu.css"/>
+<link rel="stylesheet" href="/lib/css/scale.css"/>
+<link rel="stylesheet" href="/lib/css/debug.css"/>
