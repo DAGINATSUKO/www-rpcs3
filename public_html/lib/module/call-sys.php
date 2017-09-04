fe4fb51f9fb86c2e0aa9d7b54bb03bec85ab9503
Mako <DAGINATSUKO@users.noreply.github.com>
2017-09-03 20:33:59 -0400

    Refactor

--- public_html/lib/module/call-sys.php
+++ public_html/lib/module/call-sys.php
@@ -1,8 +1,7 @@
-<script src='/lib/js/api.js'></script>
+<script src='/lib/js/jquery.js'></script>
 <script src='/lib/js/main.js'></script>
 <link rel="stylesheet" href="/lib/css/html.css"/>
 <link rel="stylesheet" href="/lib/css/anim.css"/>
 <link rel="stylesheet" href="/lib/css/main.css"/>
 <link rel="stylesheet" href="/lib/css/menu.css"/>
-<link rel="stylesheet" href="/lib/css/scale.css"/>
 <link rel="stylesheet" href="/lib/css/debug.css"/>
