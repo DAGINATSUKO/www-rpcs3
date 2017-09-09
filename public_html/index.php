d1773970ad4b7a53a0bb2e6e6f894d1a662f5d49
scribam <scribam@users.noreply.github.com>
2017-09-09 18:04:16 +0200

    Replace deprecated html tag <center> by <div style="text-align: center;">

--- public_html/index.php
+++ public_html/index.php
@@ -272,14 +272,14 @@
 						 Ad
 					</p>
 				</div>
-				<center>
-				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-				<!-- Ad -->
-				<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
+				<div style="text-align: center;">
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- Ad -->
+					<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+					<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
 					</script>
-				</center>
+				</div>
 			</div>
 			<!-- End -->
 			<?php include 'lib/module/block-requirements.php';?>
