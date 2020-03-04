1d39696f3e4de97d6db80c43e0a4167a0876d5d1
Mako <DAGINATSUKO@users.noreply.github.com>
2020-03-03 23:31:14 -0500

    Move arounds
    Moved cookie acceptance ui to menu ui, fixed roadmap class error, fixed mobile scaling of landing on small devices

--- public_html/lib/module/sys-php.php
+++ public_html/lib/module/sys-php.php
@@ -3,8 +3,3 @@
    $path .= "/lib/module/ui-main-menu.php";
    include_once($path);
 ?>
-<?php
-   $path = $_SERVER['DOCUMENT_ROOT'];
-   $path .= "/lib/module/ui-main-cookies.php";
-   include_once($path);
-?>
--- public_html/lib/module/ui-main-menu.php
+++ public_html/lib/module/ui-main-menu.php
@@ -3,6 +3,24 @@
 		<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
 	</div>
 </div>
+<div class='content-con-cookie'>
+	<div class="content-con-outside">
+		<div class="content-con-inside">
+			<div class="cookie-tx1-container">
+				<p>
+					 This website uses cookies to ensure you get the best experience. Some data may be used for analytics. <a href="/disclaimer">See full disclaimer</a>
+				</p>
+			</div>
+			<div class="cookie-btn-button">
+				<div class="cookie-ico-button">
+				</div>
+				<div class="cookie-tx1-button">
+					<span>Accept</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="window-con-theme">
 	<div class="window-bg-theme">
 	</div>
