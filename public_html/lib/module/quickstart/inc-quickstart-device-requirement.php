5ac1815270965828f2583f4b463771ef242cefee
Ani <ani-leo@outlook.com>
2024-08-08 18:49:44 +0100

    quickstart: Clarify minimum required CPU performance limitations

--- public_html/lib/module/quickstart/inc-quickstart-device-requirement.php
+++ public_html/lib/module/quickstart/inc-quickstart-device-requirement.php
@@ -41,8 +41,8 @@
 						<p>
 							 AMD - 6 cores and 12 threads or more
 						</p>
-						<span>AMD Zen 2 architecture or newer</span><br>
-						<span>AMD Threadripper 1000 and 2000 series are not recommended</span><br>
+						<span>AMD Zen 2 architecture or newer</span>
+						<br>
 						<br>
 						<p>
 							 Intel - 6 cores and 12 threads, 8 cores or more
@@ -184,13 +184,13 @@
 						<p>
 							 AMD - Any x64 CPU
 						</p>
-						<span>CPUs without AVX2 and less than 4 cores and 8 threads can be expected to run poorly</span><br>
-						<span>CPUs with only 2 cores per CCX cluster can be expected to run poorly</span><br>
+						<span>CPUs without AVX2, with 4 threads or less, or with 2 cores per CCX should perform badly</span>
+						<br>
 						<br>
 						<p>
 							 Intel - Any x64 CPU
 						</p>
-						<span>CPUs without AVX2 and less than 4 cores and 8 threads can be expected to run poorly</span>
+						<span>CPUs without AVX2, or with 4 threads or less should perform badly</span>
 					</div>
 				</div>
 			</div>
