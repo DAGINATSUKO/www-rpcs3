75390fc6f3ed2b0deda72b0981217bf89fea4698
Ani <ani-leo@outlook.com>
2025-01-18 22:03:37 +0000

    Builds: Add permalinks to latest Linux x64/arm64 AppImage

--- public_html/latest-appimage.php
+++ public_html/latest-appimage.php
@@ -2,17 +2,6 @@
 // 2018.04.06 - AniLeo
 // Permalink to latest Linux AppImage
 
-/*
-Usage:
-> wget
-wget --content-disposition https://rpcs3.net/latest-appimage
-
-> curl
-curl -JLO https://rpcs3.net/latest-appimage
-*/
-
-require "lib/compat/objects/Build.php";
-$build = Build::get_latest("linux");
-header("Content-Disposition: attachment; filename={$build->filename_linux}");
-header("Location: {$build->get_url_linux()}");
+// Redirect the old URL to the new one
+header("Location: latest-linux-x64");
 ?>
