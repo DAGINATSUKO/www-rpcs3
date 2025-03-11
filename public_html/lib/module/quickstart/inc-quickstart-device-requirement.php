8eb73509701448e1c9f09de4f2f48b1be0a795c3
Ani <ani-leo@outlook.com>
2025-03-11 22:32:16 +0000

    download: Remove macOS 13 support

--- public_html/lib/module/quickstart/inc-quickstart-device-requirement.php
+++ public_html/lib/module/quickstart/inc-quickstart-device-requirement.php
@@ -146,7 +146,7 @@
 						<p>
 							 macOS
 						</p>
-						<span>macOS 13.0+, 14.3+, 15.0+ or later</span><br>
+						<span>macOS 14.3+, 15.0+ or later</span><br>
 						<br>
 						<p>
 							 FreeBSD
@@ -289,7 +289,7 @@
 						<p>
 							 macOS
 						</p>
-						<span>macOS 13.0+, 14.3+, 15.0+ or later</span><br>
+						<span>macOS 14.3+, 15.0+ or later</span><br>
 						<br>
 						<p>
 							 FreeBSD
