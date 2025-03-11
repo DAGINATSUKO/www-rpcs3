1af01d30c7273c182fca79c8795578d27b29bf70
Ani <ani-leo@outlook.com>
2025-03-11 21:05:51 +0000

    gitignore: Remove obsolete exclusions

---
+++ public_html/rpcs4.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en-GB">
+<head>
+<title>RPCS4 - PlayStation 4 Emulator</title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD.">
+<meta name="author" content="RPCS4">
+<meta name="copyright" content="RPCS4">
+<meta http-equiv="refresh" content="0; url=https://www.youtube.com/watch?v=b-lGq5ffC3c" />
+</head>
+<script>
+window.location.replace("https://www.youtube.com/watch?v=b-lGq5ffC3c");
+</script>
+</html>
