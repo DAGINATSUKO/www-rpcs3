90cc0200080e2156044c34c777181d9b0196619d
Ani <ani-leo@outlook.com>
2026-07-26 18:06:10 +0200

    api: Add config/nointro/redump APIs

---
+++ public_html/api/nointro/updates/index.php
@@ -0,0 +1,17 @@
+<?php 
+include __DIR__."/../../../lib/compat/api/nointro.php";
+
+if (isset($_GET['api']))
+{
+    // API: v1
+    if ($_GET['api'] === "v1")
+    {
+        $results = exportNoIntroDatabase("updates");
+        header("Content-Type: application/json");
+        echo json_encode($results, JSON_PRETTY_PRINT);
+        exit();
+    }
+}
+
+http_response_code(403);
+exit("Forbidden");
